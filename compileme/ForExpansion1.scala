b49d3fe6f5e8cbd59546a9099f0d78a6bf1e74fc
Dick Wall <dick@bldc.org>
2016-08-20 13:10:26 -0700

    Simplified for expansion example

--- compileme/ForExpansion1.scala
+++ compileme/ForExpansion1.scala
@@ -1,9 +1,7 @@
-object ForExpansion1 extends App {
+object ForExpansion1 {
   val mults = for {
     i <- 1 to 3 // flatMap
     j <- 1 to 3 // flatMap
     k <- 1 to 3 // map
   } yield i*j*k
-
-  println(mults)
 }
